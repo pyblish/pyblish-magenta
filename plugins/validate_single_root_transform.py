18b716d8ffa14875b67e1d7ea04cb3e9815ed73f
Roy Nieterau <roy_nieterau@hotmail.com>
2015-02-03 08:54:48 +0100

    Added single root node validator

---
+++ plugins/validate_single_root_transform.py
@@ -0,0 +1,28 @@
+import pyblish.api
+from maya import cmds
+
+def getRootNode(longPath):
+    return '|' + longPath.lstrip('|').split("|", 1)[0]
+
+
+class ValidateSingleRootTransform(pyblish.api.Validator):
+    """ Validate all nodes are in a single root """
+    families = ['rig']
+    hosts = ['maya']
+    category = 'rig'
+    version = (0, 1, 0)
+
+    def process_instance(self, instance):
+        """Process all the nodes in the instance 'objectSet' """
+        member_nodes = cmds.sets(instance.name, q=1)
+
+        # ensure long names so we can get root by strings (fastest way?)
+        member_nodes_paths = cmds.ls(member_nodes, type='dagNode', long=True)
+
+        roots = set()
+        for node in :
+            root = getRootNode(node)
+            roots.add(root)
+
+        if roots:
+            raise ValueError("Multiple root nodes found named shapes found: {0}".format(list(roots)))
