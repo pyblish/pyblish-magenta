b6429c6197ca2ca831de2b19ea4791107349d9a6
Marcus Ottosson <konstruktion@gmail.com>
2015-06-13 15:07:35 +0100

    Added support for Dash

--- integration/maya/userSetup.py
+++ integration/maya/userSetup.py
@@ -1,3 +1,5 @@
+import os
+from maya import mel
 
 try:
     import pyblish_magenta
@@ -5,3 +7,15 @@ try:
 
 except ImportError as e:
     print "pyblish_magenta: Could not load kit: %s" % e
+
+
+# Set project
+development_dir = os.environ.get("DEVELOPMENTDIR")
+
+if os.name == "nt":
+    # MEL can't handle backslash
+    development_dir = development_dir.replace("\\", "/")
+
+if development_dir:
+    print("Setting Magenta development directory to: %s" % development_dir)
+    mel.eval('setProject \"' + development_dir + '\"')
