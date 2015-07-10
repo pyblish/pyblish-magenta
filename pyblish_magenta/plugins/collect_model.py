2a34f518f350c2d1bceda790cc73793a99dc46a6
Marcus Ottosson <konstruktion@gmail.com>
2015-07-10 12:20:18 +0100

    Filtering output to shapes only.

--- pyblish_magenta/plugins/collect_model.py
+++ pyblish_magenta/plugins/collect_model.py
@@ -20,17 +20,20 @@ class CollectModel(pyblish.api.Collector):
         # Get the root transform
         self.log.info("Model found: %s" % name)
         assembly = "|{name}_GRP".format(name=name)
-        root_transform = cmds.ls(assembly,
-                                 objectsOnly=True,
-                                 type="transform")
 
-        assert root_transform, (
+        assert cmds.objExists(assembly), (
             "Model did not have an appropriate assembly: %s" % assembly)
 
-        self.log.info("Root transform determined to be: %s" % assembly)
-        root_transform = root_transform[0]
+        self.log.info("Capturing instance contents: %s" % assembly)
+        with pyblish_maya.maintained_selection():
+            cmds.select(assembly)
+            nodes = cmds.file(exportSelected=True,
+                              preview=True,
+                              constructionHistory=True,
+                              force=True)
 
-        shapes = cmds.ls(root_transform,
+        self.log.info("Reducing nodes to shapes only")
+        shapes = cmds.ls(nodes,
                          noIntermediate=True,
                          shapes=True,
                          long=True,
@@ -38,15 +41,7 @@ class CollectModel(pyblish.api.Collector):
 
         assert shapes, "Model did not have any shapes"
 
-        instance = context.create_instance(name=name,
-                                           family="model")
-
-        self.log.info("Capturing instance contents: %s" % assembly)
-        with pyblish_maya.maintained_selection():
-            cmds.select(shapes)
-            instance[:] = cmds.file(exportSelected=True,
-                                    preview=True,
-                                    constructionHistory=True,
-                                    force=True)
+        instance = context.create_instance(name=name, family="model")
+        instance[:] = shapes
 
         self.log.info("Successfully collected %s" % name)
