f1e0d7e248111c312547ea7c517629c5a32ff2c4
Marcus Ottosson <konstruktion@gmail.com>
2015-07-14 07:28:54 +0100

    Updating suite

--- run_testsuite.py
+++ run_testsuite.py
@@ -1,13 +1,10 @@
 import os
 import sys
-from pyblish.vendor import nose
+import nose
 
 # Expose Pyblish Magenta to PYTHONPATH
 path = os.path.dirname(__file__)
 sys.path.insert(0, path)
-
-import pyblish_magenta
-pyblish_magenta.setup()
 
 if __name__ == '__main__':
     argv = sys.argv[:]
