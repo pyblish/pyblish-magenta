a704f26411f977128fd537540d3926bb683ff43a
Marcus Ottosson <konstruktion@gmail.com>
2015-08-14 14:38:28 +0100

    Bugfix against buggy inspect module

--- pyblish_magenta/plugins/wrap_alembic.py
+++ pyblish_magenta/plugins/wrap_alembic.py
@@ -1,7 +1,15 @@
+"""Wrap bare alembic files into a Maya ASCII
+
+Published assets are designed for Maya referencing, but alembics
+have problems with this (need proof) and so they are first
+embedded into a Maya scene file that is later referenced, in place
+of referencing the bare alembic.
+
+"""
+
 import os
 import json
 import shutil
-import inspect
 import tempfile
 import subprocess
 import contextlib
@@ -11,8 +19,8 @@ import pyblish.api
 CREATE_NO_WINDOW = 0x08000000
 
 
-class WrapAlembic(pyblish.api.Extractor):
-    """Wrap alembic files in a Maya scene
+class WrapAlembics(pyblish.api.Extractor):
+    """Wrap alembic files with a Maya scene
 
     This plug-in launches Maya Standalone in a subprocess
     and wraps each pointcache instance into an additional
@@ -23,8 +31,10 @@ class WrapAlembic(pyblish.api.Extractor):
 
     """
 
+    label = "Wrap Alembics"
     families = ["pointcache"]
     order = pyblish.api.Extractor.order + 0.1
+    optional = True
 
     def process(self, context):
 
@@ -42,70 +52,97 @@ class WrapAlembic(pyblish.api.Extractor):
 
             paths.append(extract_dir)
 
-        source = "%s\ncode()" % inspect.getsource(code)
-        source = source.format(paths=json.dumps(paths))
+        source = code.format(paths=json.dumps(paths))
 
         self.log.info("Running source in Maya Standalone: %s" % source)
         self.log.info("temp_file: %s" % temp_file)
 
         with temp_file(source) as filename:
             self.log.debug("Starting subprocess..")
+            
+            # Don't include Pyblish nor Magenta
+            environment = os.environ.copy()
+            environment.pop("PYTHONPATH", None)
+
             popen = subprocess.Popen(["mayapy", filename],
                                      stdout=subprocess.PIPE,
                                      stderr=subprocess.STDOUT,
-                                     creationflags=CREATE_NO_WINDOW)
+                                     creationflags=CREATE_NO_WINDOW,
+                                     env=environment)
+
+            # Include full output, including any traceback
             for line in iter(popen.stdout.readline, b""):
                 self.log.debug(line.strip())
+
             popen.communicate()  # Block till finished
+
             assert popen.returncode == 0, (
                 "An error occured, see debug log messages for details")
 
+            self.log.info("Alembics wrapped successfully")
 
-def code():
-    """Wrap `alembic` in a Maya ASCII file
 
-    DO NOT RUN DIRECTLY.
 
-    This function is serialised, written and run through
-    maya.standalone in the plug-in below
 
-    """
+code = r"""
+# Wrap alembic in a Maya ASCII file
+#
+# This is written and run through maya.standalone
+#
+# Note:
+#   Couldn't use inspect.getsource() on a function, due to a bug
+#   in caching: https://bugs.python.org/issue1218234
+#   Causing the getsource() to not return the latest update
+#   on plug-in reload.
+#
 
-    import json
+import os
+import sys
+import json
 
-    from maya import cmds
-    from maya import standalone
+print("Initialising Maya..")
+import pymel.core
+from maya import cmds
 
-    print("Initialising Maya..")
-    standalone.initialize()
+cmds.loadPlugin('AbcImport', quiet=True)
 
-    cmds.loadPlugin('AbcImport', quiet=True)
+paths = {paths}  # Provided at run-time
 
-    paths = {paths}  # Provided at run-time
+for path in paths:
+    path = os.path.realpath(path)
+    assert os.path.isdir(path)
+    for alembic in os.listdir(path):
+        alembic = os.path.join(path, alembic)
+        if not alembic.endswith(".abc"):
+            continue
 
-    for path in paths:
-        path = os.path.realpath(path)
-        assert os.path.isdir(path)
-        for alembic in os.listdir(path):
-            alembic = os.path.join(path, alembic)
-            if not alembic.endswith(".abc"):
-                continue
+        cmds.file(new=True, force=True)
 
-            cmds.file(new=True, force=True)
+        print("Importing %s" % alembic)
+        cmds.AbcImport(alembic,
+                       mode="import",
+                       fitTimeRange=True,
+                       setToStartFrame=True)
 
-            print("Importing %s" % alembic)
-            cmds.AbcImport(alembic, mode="open")
+        # Sanity check
+        for node in cmds.ls(type="AlembicNode"):
+            print("%s.startFrame: %s" % (node, cmds.getAttr(node + ".startFrame")))
 
-            wrapper = os.path.splitext(alembic)[0] + ".ma"
-            wrapper = os.path.join(path, wrapper).replace("\\", "/")
-            
-            print("Exporting to %s" % wrapper)
-            cmds.file(rename=wrapper)
+        wrapper = os.path.splitext(alembic)[0] + ".ma"
+        wrapper = os.path.join(path, wrapper).replace("\\", "/")
+        
+        print("Exporting to %s" % wrapper)
+        cmds.file(rename=wrapper)
 
-            cmds.file(wrapper, exportAll=True, force=True, type="mayaAscii")
-            print("Exported successfully")
+        cmds.file(save=True,
+                  force=True,
+                  type="mayaAscii",
+                  constructionHistory=True)
+        print("Exported successfully")
 
-    os.exit(0)
+sys.exit(0)
+
+"""
 
 
 @contextlib.contextmanager
