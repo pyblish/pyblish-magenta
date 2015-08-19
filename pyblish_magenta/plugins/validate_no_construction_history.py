312d5169e7c51b0724d0f6e6c68103b8a9b58a9a
Marcus Ottosson <konstruktion@gmail.com>
2015-08-19 08:32:25 +0100

    Alternative construction history identifier
    Everything not a DAG node is deemed construction history.

--- pyblish_magenta/plugins/validate_no_construction_history.py
+++ pyblish_magenta/plugins/validate_no_construction_history.py
@@ -5,30 +5,17 @@ from maya import cmds
 class ValidateNoConstructionHistory(pyblish.api.Validator):
     """Ensure no construction history exists on the nodes in the instance"""
 
-    families = ['model']
-    hosts = ['maya']
-    category = 'geometry'
-    version = (0, 1, 0)
+    families = ["model"]
+    hosts = ["maya"]
+    category = "geometry"
     label = "No Construction History"
 
     def process(self, instance):
         """Process all the nodes in the instance"""
-        if not instance:
-            # Maya's listHistory errors out on an empty list so we return if not anything in the instance.
-            return
+        has_history = list(
+            node for node in instance
+            if "dagNode" not in cmds.nodeType(node, inherited=True)
+        )
 
-        # quick check first
-        if not cmds.listHistory(instance, pruneDagObjects=True):
-            return
-
-        # identify the invalid nodes
-        invalid = []
-        for node in instance:
-            if cmds.listHistory(node, pruneDagObjects=True):
-                invalid.append(node)
-        if invalid:
-            raise ValueError("Nodes found with construction history: {0}".format(invalid))
-
-    def repair(self, instance):
-        """Delete history on all nodes in the instance"""
-        cmds.delete(instance, channelHistory=True)
+        assert not has_history, (
+            "The following nodes have construction history: %s" % has_history)
